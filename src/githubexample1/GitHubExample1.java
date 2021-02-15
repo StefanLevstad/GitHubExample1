6bb60a455933a4ee7f4cdee2c932119f113dfbba
Stefan Levstad <stefan.levstad.mte21lin@tucsweden.se>
2021-02-15 08:50:57 +0100

    Init commit. Lägger till projektet till Repository

---
+++ src/githubexample1/GitHubExample1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubexample1;
+
+/**
+ *
+ * @author stefa
+ */
+public class GitHubExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Detta är ett GitHub exempel!");
+    }
+    
+}
